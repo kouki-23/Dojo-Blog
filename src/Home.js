4e502ce722a01368a01483a07c5cb7cbc75b36e2
kouki-23 <khouloudzidi18@gmail.com>
2023-06-24 01:03:43 -0800

    commit 1

---
+++ src/Home.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import BlogList from './BlogList';
+const Home = () => { 
+    const [blogs, setBlogs] = useState(null);
+    const [isPending , setIsPending] = useState(true);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    } 
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+          .then(res => {
+            return res.json();
+         })
+          .then(data => {
+            setBlogs(data);
+            setIsPending(false);
+         });
+    },[]);
+    
+    return (
+        <div className="home">
+            {isPending && <div>loading....</div>}
+           {blogs && <BlogList blogs={blogs} title="ALl BLogs!" handleDelete={handleDelete}/> } 
+
+        </div>
+      );
+}
+  
+export default Home;
