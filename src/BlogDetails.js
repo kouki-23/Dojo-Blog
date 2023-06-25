8a65a1e34eb5e2576303dec9ba40febf1a864347
kouki-23 <khouloudzidi18@gmail.com>
2023-06-25 08:03:56 -0800

    commit 2

---
+++ src/BlogDetails.js
@@ -0,0 +1,33 @@
+import { useHistory, useParams } from "react-router-dom";
+import useFetch from "./useFtech";
+
+const BlogDetails = () => {
+    const { id } = useParams();
+    const {data : blog ,error, isPending } = useFetch('http://localhost:8000/blogs/' + id); 
+    const handleClick =() => {
+        fetch('http://localhost:8000/blogs/'+ blog.id ,{
+        method : 'DELETE'
+    }).then(() => {
+            History.push('/');
+        })
+
+    }
+    return (
+
+        <div className="blog-details">
+           {isPending && <div>loading...</div>}
+           {error && <div>{error}</div>}
+           {blog && (
+            <article>
+                <h2>{blog.title}</h2>
+                <p>Written by {blog.author} </p>
+                <div>{ blog.body }</div>
+                <button onClick={handleClick}>delete</button>
+            </article>
+           )}
+        </div>
+    );
+}
+
+ 
+export default BlogDetails;
--- src/useFtech.js
+++ src/useFtech.js
@@ -5,9 +5,10 @@ const useFetch = (url) => {
     const [isPending , setIsPending] = useState(true);
     const [error,setError] = useState(null);
 
-    useEffect(() => {
+    useEffect(() => { 
+      const abortCont = new AbortController();
         setTimeout(() => {
-          fetch(url)
+          fetch(url, {signal : abortCont.signal})
             .then(res => {
             console.log(res); 
             if (!res.ok) {
@@ -21,11 +22,17 @@ const useFetch = (url) => {
               setError(null);
           })
           .catch(err => {
-            setIsPending(false);
-            setError(err.message);
-          })
-        
-          },1000) ;     
+            if(err.name === 'AbortError'){
+              console.log('fetch aborted')
+            } else{
+              setIsPending(false);
+              setError(err.message);
+            }
+            })
+          },1000) ;  
+         
+         
+        return () => abortCont.abort();   
       },[url]); 
       return {data,isPending,error}
 } 
